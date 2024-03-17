eaaebb4ae239c9ba69ed6844b847cf6c078837fb
PraveenKumar263 <PraveenKumar263>
2024-03-17 22:37:02 +0800

    update code to answer task 6 qns

--- day-06/script.js
+++ day-06/script.js
@@ -23,28 +23,55 @@ request.onload = function () {
     var res = JSON.parse(request.response)
     console.log(res)
 
-    // Print all hte counties name in the array
-    var data = res.map((ele)=>ele.name.common);
-    // console.log(data);
+    // Get all the countries from Asia continent /region using Filter function
+    var countries_asia = res.filter(ele=>ele.region==='Asia').map(ele=>ele.name.common);
+    console.log(countries_asia);
 
-    // // print country names which are in europe region
-    // var data2 = res.reduce((acc, ele) => {
-    //     if (ele.region === 'Europe') {
-    //         acc.push(ele.name.common);
-    //     }
-    //     return acc;
-    // }, []);
-    // // alternative
-    var data2=res.filter((ele)=>ele.region === 'Europe').map((ele)=>ele.name.common);
-    // console.log(data2);
+    // Get all the countries with a population of less than 2 lakhs using Filter function
+    var pop_less_200k = res.filter(ele=>ele.population < 200000).map(ele=>ele.name.common);
+    console.log(pop_less_200k);
 
-    // print capital of those coutnried whose area is more than 100000 sq km
-
-    var capital_100k = res.filter((ele)=>ele.area>100000).map((ele)=>(ele.capital));
-    console.log(capital_100k);
+    // Print the following details name, capital, flag, using forEach function
+    res.forEach(ele => {
+        console.log(ele.name.common);
+        if(ele.capital) {
+            console.log(ele.capital['0']);
+        }
+        console.log(ele.flag);
+    });
 
     // find the sum of total population for all countries
     var total_pop = res.reduce((acc,ele)=>acc+ele.population, 0);
     console.log(total_pop);
+
+    // Print the country that uses US dollars as currency.
+    var us_dollars_countries = res.filter(ele=> {
+        if(ele.currencies) {
+            return ele.currencies.USD;
+        }
+    }).map(ele=>ele.name.common);
+    console.log(us_dollars_countries);
+
+    // // Print all the counties name in the array
+    // var data = res.map((ele)=>ele.name.common);
+    // // console.log(data);
+
+    // // // print country names which are in europe region
+    // // var data2 = res.reduce((acc, ele) => {
+    // //     if (ele.region === 'Europe') {
+    // //         acc.push(ele.name.common);
+    // //     }
+    // //     return acc;
+    // // }, []);
+    // // // alternative
+    // var data2=res.filter((ele)=>ele.region === 'Europe').map((ele)=>ele.name.common);
+    // // console.log(data2);
+
+    // // print capital of those coutnried whose area is more than 100000 sq km
+
+    // var capital_100k = res.filter((ele)=>ele.area>100000).map((ele)=>(ele.capital));
+    // console.log(capital_100k);
+
+
 
 }
